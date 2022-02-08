c31966e8581a6fa18146457c47817cbeda93fb27
codingloop <ishunfs@gmail.com>
2022-02-08 21:52:53 +0530

    Fix material UI SSR

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,24 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
+import useStyles from "../utils/styles";
 
 function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Ecommerce Portal</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Ecommerse Portal</Typography>
+          <Typography>Ecommerce Portal</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. Next-Ecommerce</Typography>
+      </footer>
     </div>
   );
 }
