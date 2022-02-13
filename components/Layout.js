0df525aacc2df62476e663602f03401892099923
codingloop <ishunfs@gmail.com>
2022-02-13 20:20:56 +0530

    Theme adding

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import {
   AppBar,
   Container,
+  createMuiTheme,
+  CssBaseline,
   Link,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from "@material-ui/core";
@@ -11,6 +14,29 @@ import React from "react";
 import useStyles from "../utils/styles";
 
 function Layout({ title, description, children }) {
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
   const classes = useStyles();
   return (
     <div>
@@ -20,30 +46,33 @@ function Layout({ title, description, children }) {
         </title>
         {description && <meta name="descrption" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>
-                Ecommerce Portal
-              </Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  Ecommerce Portal
+                </Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Next-Ecommerce</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Next-Ecommerce</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
