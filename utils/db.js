6304dd355b6858a5b0c9bd6463d8515a11f71cb1
codingloop <ishunfs@gmail.com>
2022-02-14 22:31:15 +0530

    Adding mongo db utils

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+async function connect() {
+  if (connection.isConnected) {
+    console.log("already connected");
+    return;
+  }
+  if ((mongoose.isConnected = mongoose.connections[0].readyState)) {
+    if (connection.isConnected === 1) {
+      console.log("use prev connection");
+      return;
+    }
+    await mongoose.disconnect();
+  }
+
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+  });
+  console.log("new connection");
+  connection.isConnected = db.connections[0].readyState;
+}
+
+async function disconnect() {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === "production") {
+      await mongoose.disconnect();
+    } else {
+      console.log("not disconnected");
+    }
+  }
+}
+
+const db = { connect, disconnect };
+
+export default db;
